514eea4e6477c9589cf0ae89d5bdfca9e8b6ea2d
Matias Spacech <matiascruse@gmail.com>
2024-03-13 23:45:04 -0300

    Terminado la herencia de templates y ver_auto

--- pagina/urls.py
+++ pagina/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from pagina.views import pagina, agregar_autos ,lista_de_autos
+from pagina.views import pagina, agregar_autos ,lista_de_autos, ver_auto
 
 urlpatterns = [
     path('', pagina, name = 'principal'),
     path('autos/nuevo', agregar_autos, name = 'AgregarAutos'),
-    path('autos', lista_de_autos, name = 'Autos')        
+    path('autos', lista_de_autos, name = 'Autos'), 
+    path('autos/<int:id_auto>' , ver_auto, name = 'ver_auto' )       
 ]
--- pagina/views.py
+++ pagina/views.py
@@ -4,15 +4,15 @@ from pagina.models import Auto
 from pagina.forms import formularioAutos, BuscarAuto
 # Create your views here.
 def pagina(request):
-    return render(request , 'principal.html')
+    return render(request , 'pagina/principal.html')
+    #return render(request , 'base.html')
 
 def lista_de_autos(request):
-    autos= Auto.objects.all()
     formulario = BuscarAuto(request.GET)
     if formulario.is_valid():
         modelo_a_buscar = formulario.cleaned_data.get('modelo')
         autos = Auto.objects.filter(modelo__icontains=modelo_a_buscar)
-    return render(request, 'autos.html', {'autos': autos , 'formulario': formulario})
+    return render(request, 'pagina/autos.html', {'autos': autos , 'formulario': formulario})
     
 def agregar_autos (request):
     formulario = formularioAutos()
@@ -28,6 +28,8 @@ def agregar_autos (request):
             auto.save()
             return redirect("Autos")
         
-    return render(request, 'agregar-autos.html', {'formulario': formulario})
+    return render(request, 'pagina/agregar-autos.html', {'formulario': formulario})
     
- 
+def ver_auto(request, id_auto):
+    auto = Auto.objects.get(id=id_auto)
+    return render(request, 'pagina/ver_auto.html', {'auto':auto})
